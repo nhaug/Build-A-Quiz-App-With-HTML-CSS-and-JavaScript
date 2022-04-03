8c139e9e095e9856cd739ad80a67d840b6c3a9e4
nhaug <mail@n-haug.de>
2022-04-03 23:45:38 +0200

    disabled nun im html setzen für default

--- end.js
+++ end.js
@@ -1,13 +1,15 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn')
-saveScoreBtn.disabled = true;
-    
-    const mostRecentScore = localStorage.getItem("mostRecentScore");
-    console.log(mostRecentScore);
-    finalScore.innerText = mostRecentScore;
+// saveScoreBtn.disabled = true;
 
-username.addEventListener( "keyup", () => {
-    console.log(username.value);
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+    saveScoreBtn.disabled = !username.value;
+})
+
+username.addEventListener("change", () => {
     saveScoreBtn.disabled = !username.value;
 })
 
